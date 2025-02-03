a1cf7516ab729f5639407d79a6f26bca60dc4615
Sarfaraj525 <sarfaraj525@gmail.com>
2025-02-03 16:59:42 +0600

    Invoice payment & Bill-dark mode className few changes

--- src/pages/InvoiceList/InvoiceList.tsx
+++ src/pages/InvoiceList/InvoiceList.tsx
@@ -167,7 +167,7 @@ export default function InvoiceList() {
 
 
       {/* Pagination */}
-<div className="flex justify-end mt-4 gap-2">
+<div className="flex justify-end mt-4 gap-2 mr-2">
   <button className="px-4 py-2 border rounded bg-gray-200 dark:bg-gray-800  dark:text-gray-200 dark:border-gray-500 flex items-center gap-2 hover:bg-gray-600 dark:hover:bg-gray-400  transition">
     <FaChevronLeft className="text-gray-600" />
   </button>
