2e0b6eebe2359596562b1f192b214517a9133ba9
Sarfaraj525 <sarfaraj525@gmail.com>
2025-02-23 12:37:30 +0600

    Ecommerce Notifications few changes

--- src/pages/eCommerce/ECommerceNotifications.tsx
+++ src/pages/eCommerce/ECommerceNotifications.tsx
@@ -65,7 +65,7 @@ const ECommerceNotifications = () => {
 
   return (
     <div className="p-6 bg-gray-50 mt-20 ml-6 dark:bg-gray-900 max-w-full rounded-lg shadow-2xl">
-      <div className="flex bg-gray-100 dark:bg-gray-900">
+      <div className="flex  dark:bg-gray-900">
         {/* Sidebar */}
         <aside className="w-64 p-6">
           <h2 className="text-lg font-semibold mb-4">Getting Started</h2>
@@ -88,17 +88,17 @@ const ECommerceNotifications = () => {
 
         {/* Right Side Content */}
         {activeTab === "Notifications" && (
-          <div className="flex-1 p-6">
+          <div className="flex-1 p-6 bg-white">
             {Object.entries(notifications).map(([category, types]) => (
               <div
                 key={category}
-                className="mb-6 bg-white dark:bg-gray-800 p-4 rounded-lg shadow"
+                className="mb-6 bg-white dark:bg-gray-800 p-4 rounded-lg "
               >
                 <h3 className="text-lg font-semibold mb-3 capitalize">
                   {category}
                 </h3>
                 <div className="border rounded-lg">
-                  <div className="grid grid-cols-3 gap-4 p-4 bg-gray-100 dark:bg-gray-700 font-semibold">
+                  <div className="grid grid-cols-3 gap-4 p-4  dark:bg-gray-700 font-semibold">
                     <span>TYPE</span>
                     <span className="text-center">EMAIL</span>
                     <span className="text-center">APP</span>
@@ -145,8 +145,12 @@ const ECommerceNotifications = () => {
               </div>
             ))}
 
-            {/* Save & Discard Buttons */}
-            <div className="flex justify-end mt-6 gap-4">
+            
+          </div>
+        )}
+      </div>
+      {/* Save & Discard Buttons */}
+      <div className="flex justify-end mt-6 gap-4">
               <button className="px-4 py-2 bg-gray-300 text-gray-700 rounded-lg">
                 Discard
               </button>
@@ -154,9 +158,6 @@ const ECommerceNotifications = () => {
                 Save Changes
               </button>
             </div>
-          </div>
-        )}
-      </div>
     </div>
   );
 };
